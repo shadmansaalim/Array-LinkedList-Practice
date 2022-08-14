562f3e00bca12e4ba961e4904f6f39fac561c9ff
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-08-14 18:32:04 +1000

    Doubly linked list problem solved 16

--- 16.cpp
+++ 16.cpp
@@ -1,10 +1,6 @@
 
 /*
-Write a function named move_tail() which will move the tail to the first position of the list.
-For example: If the list looks like this,
-List: 2 -> 4 -> 1 -> 10 -> 5
-After the operation the list will look like this,
-
+Write a function insert_node() to insert a value at the end of a doubly linked list. If there are no nodes, it will insert a head.
 */
 
 #include <iostream>
